f4e1de85e0e6e64ec336afefd8ecbcb4756ac5a1
liraz17-meet <liraz17@meet.mit.edu>
2017-02-13 18:19:43 +0200

    initial commit

---
+++ model.py
@@ -0,0 +1,47 @@
+
+from sqlalchemy import Column,Integer,String, DateTime, ForeignKey, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine, func
+from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+Base = declarative_base()
+
+class Questions(Base):
+	__tablename__ = 'questions'
+	id = Column(integer, primary_key=True)
+	subject = Column(string)
+	sub_subject = Column(string)
+	image = 
+	description = Column(TextArea)
+	answers = relationship("Answers", back_populates="questions")
+
+
+class Answers(Base):
+	__tablename__ = 'answers'
+	id = Column()
+	image = 
+	description = 
+	question_id = Column(Integer, ForeignKey=True)
+	questions = relationship("Questions", back_populates="answers")
+
+class Users(Base):
+	__tablename__ = 'users'
+	id = Column(integer, primary_key=True)
+	name = Column(string)
+	password = Column(string) 
+	email = Column(string) 
+	answerscount = Column(string) 
+
+Liraz = User(id = 2. name = Liraz Shahar, password = pass, email = )
+
+
+
+
+
+engine = create_engine('sqlite:///fizzBuzz.db')
+
+
+Base.metadata.create_all(engine)
---
+++ webapp.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+	return render_template('homework.html')
+
+@app.route("/math")
+def math():
+	return render_template('math.html')
+
+
+@app.route("/math/geometry")
+def geometry():
+	return render_template('geometry.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
