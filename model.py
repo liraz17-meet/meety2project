494c94a87d6dec6a847e43778dc508fbcd1d8801
malak18-meet <malak.jabarin@gmail.com>
2017-02-14 10:39:14 +0200

    vmiks

--- model.py
+++ model.py
@@ -35,12 +35,6 @@ class Users(Base):
 	email = Column(string) 
 	answerscount = Column(string) 
 
-class Upload(Base):
-	__tablename__ = 'upload'
-	id = Column(integer, primary_key=True)
-	image = Column(string)
-	description = Column(string) 
-	category = Column(string) 
 
 
 Liraz = User(id = 2. name = Liraz Shahar, password = pass, email = )
--- webapp.py
+++ webapp.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from model import Base, Questions, answers, users
+
+
+engine = create_engine('sqlite:///fizzBuzz.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine, autoflush=False)
+session = DBSession()
+
+current_directory = os.getcwd()
+UPLOAD_FOLDER = os.path.join(current_directory, 'static/uploads')
+allowed_extensions = set(['jpg', 'jpeg', 'png'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def homepage():
@@ -15,9 +28,27 @@ def geometry():
 	return render_template('geometry.html')
 
 @app.route("/upload")
+def valid_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in allowed_extensions
 def upload():
-	return render_template('upload.html')
+	file = request.files['file']
+	if request.method == 'POST' and file and valid_file(file.filename) and session.get('id')!=None:
+		path = os.path.join(aoo.config['UPLOAD_FOLDER'], filename)
+		file.save(path)
+		user = DBsession.query(Users).filter_by(id = session['id']).first()
+		description = request.form['description']
+		subject = request.form['subject']
+		sub_subject = request.form['sub_subject']
+		question = Questions(user_id= user.id, subject, file_name = "uploads/" + filename,subject = request.form['subject'], 
+		DBsession.add(question)
+		DBsession.commit()
 
+
+		return render_template('homepage.html')
+
+	else:
+		redirect(url_for('upload'))
+		
 
 if __name__ == '__main__':
 	app.run(debug=True)
